64ff6a6de4d355ed9a92b25b2b46251edf5cbd6d
Matysse <mathis.jung@blgcloud.com>
2026-03-24 17:44:07 +0100

    added snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		'folke/snacks.nvim',
+		opts = {
+			dashboard = { enabled = true },
+			indent    = { enabled = true },
+			scratch   = { enabled = true },
+		},
+		-- keys = {
+		-- 	{
+		-- 		"<leader>.",
+		-- 		function ()
+		-- 			Snacks.scratch()
+		-- 		end,
+		-- 		desc = "Toggle Scratch Buffer"
+		-- 	},
+		-- 	{
+		-- 		"<leader>S",
+		-- 		function ()
+		-- 			Snacks.scratch.select()
+		-- 		end,
+		-- 		desc = "Select Scratch Buffer"
+		-- 	}
+		-- }
+	}
+}
