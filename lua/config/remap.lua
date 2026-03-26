bf93ab4ebb6096a04b3b0e69d410e6baddb95595
Matysse <mathis.jung@blgcloud.com>
2026-03-26 09:38:20 +0100

    remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,10 @@
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "U", "<C-r>")
