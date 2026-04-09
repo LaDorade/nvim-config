d968d6f692e0c5620ca987d4b7a5f04a030bc77e
Matysse <mathis.jung@blgcloud.com>
2026-04-09 11:43:05 +0200

    oil.nvim

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) -- when not using Oil.nvim
 
 -- Credits to theprimeagen
 -- greatest remap ever
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,15 +1,18 @@
 return {
-	-- {
-	-- 	'stevearc/oil.nvim',
-	-- 	---@module 'oil'
-	-- 	---@type oil.SetupOpts
-	-- 	-- Optional dependencies
-	-- 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	-- 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	-- 	lazy = false,
-	-- 	opts = {
-	-- 		default_file_explorer = true,
-	-- 	},
-	-- }
+	{
+		'stevearc/oil.nvim',
+		---@module 'oil'
+		---@type oil.SetupOpts
+		-- Optional dependencies
+		-- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
+		opts = {
+			default_file_explorer = true,
+		},
+		keys = {
+			{'<leader>pv', '<cmd>Oil<cr>', 'Open Oil.nvim'}
+		}
+	}
 }
