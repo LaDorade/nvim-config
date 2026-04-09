1b2df255bf4cb330e05ec218b51b3ed35a9ceaa0
Matysse <mathis.jung@blgcloud.com>
2026-04-09 10:06:23 +0200

    telescope grep_string bind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,11 +16,18 @@ return {
 			}
 		},
 		keys = {
-			{ '<leader>p' , ""}, -- disable <leader>p because its annoying when I only type the first char
+			{ '<leader>p' , ""}, -- disable <leader>p because it can be annoying if I don't tape fast enough
 			{ '<leader>pf', "<cmd>Telescope find_files<cr>", { desc = 'Telescope find files' } },
 			{ '<leader>pg', "<cmd>Telescope live_grep<cr>" , { desc = 'Telescope live grep' }},
 			{ '<leader>pb', "<cmd>Telescope buffers<cr>"   , { desc = 'Telescope buffers' }},
 			{ '<leader>ph', "<cmd>Telescope help_tags<cr>" , { desc = 'Telescope help tags' }},
+			{ '<leader>ps', function ()
+				local builtin = require('telescope.builtin')
+				builtin.grep_string({search = vim.fn.input({
+					prompt = "Grep > ",
+					default = vim.fn.expand("<cword>"),
+				})})
+			end, { desc = 'Telescope grep_string better' }},
 		},
 	}
 }
