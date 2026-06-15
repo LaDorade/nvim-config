a5a16d7d1b1abc6b6a9f06d8da486a9eaacaa137
Matysse <mathis.jung@blgcloud.com>
2026-06-15 09:43:24 +0200

    adjust mini config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,32 @@
 return {
 	{
-		'nvim-mini/mini.nvim', version = '*',
+		'nvim-mini/mini.surround', version = '*',
 		config = function ()
 			require('mini.surround').setup()
+		end,
+	},
+
+	{
+		'nvim-mini/mini.pairs', version = '*',
+		config = function ()
 			-- vim.keymap.set('v', 'sa', surround)
 			-- sd, sr, sf, sh
-			-- require('mini.pairs').setup()
-		end
+			require('mini.pairs').setup({
+				mappings = {
+					['('] = { neigh_pattern = '^[^%a%p]' }
+				},
+			})
+		end,
 	},
+
+	{
+		'nvim-mini/mini.files', version = '*',
+		config = function ()
+			require('mini.files').setup({
+				mappings = {
+					go_in = '<cr>',
+				},
+			})
+		end
+	}
 }
